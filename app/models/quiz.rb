8fc121ff125e33b2940273a1115d56728930540d
hansgut <hansgut@outlook.com>
2018-04-05 12:20:26 +0300

    some fix

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,4 +1,5 @@
 class Quiz < ApplicationRecord
+  validates :title, presence: true
   has_many :questions, dependent: :destroy
   has_many :attempts
   accepts_nested_attributes_for :questions, reject_if: lambda { |attributes| attributes['title'].blank? }, allow_destroy: true
