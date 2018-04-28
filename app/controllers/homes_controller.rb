54e89fccaf4cb336cc981ae996429e84965aa5ea
hansgut <hansgut@outlook.com>
2018-04-28 04:35:57 +0300

    lal

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,2 +1,8 @@
 class HomesController < ApplicationController
+  def index
+    @last_theory = Theory.last
+    @last_practice = Practice.last
+    @last_quiz = Quiz.last
+    @last_determination = Determination.last
+  end
 end
