7787a050e4b2963067db0111d665d68def98bc53
hansgut <hansgut@outlook.com>
2018-06-19 11:03:10 +0300

    attp

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,7 +1,7 @@
 class Quiz < ApplicationRecord
   validates :title, presence: true
   has_many :questions, dependent: :destroy
-  has_many :attempts
+  has_many :attempts, dependent: :destroy
   accepts_nested_attributes_for :questions, reject_if: lambda { |attributes| attributes['title'].blank? }, allow_destroy: true
   def self.search(search)
     where("title LIKE ?", "%#{search}%")
